4a788b0f1cf7970c564f683a74f170c57db21859
notCroptu <martinsmariana1972@gmail.com>
2024-05-08 23:40:15 +0100

    Ex 6 Filepower2

---
+++ FilePower2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace FilePower2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = args[0];
+
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                string line;
+
+                while (true)
+                {
+                    line = Console.ReadLine();
+                    if (line == "")
+                    {
+                        break;
+                    }
+                    sw.WriteLine(line);
+                }
+            }
+        }
+    }
+}
