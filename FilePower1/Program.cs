2aee5991d24d22698b7b315b6230b18f2fb43668
notCroptu <martinsmariana1972@gmail.com>
2024-05-08 23:30:36 +0100

    Ex 5 FilePower1

---
+++ FilePower1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = args[0];
+
+            Queue<string> queue = new Queue<string>();
+            string line;
+
+            while (true)
+            {
+                line = Console.ReadLine();
+
+                if (line == "")
+                {
+                    break;
+                }
+                queue.Enqueue(line);
+            }
+
+            if ( File.Exists(file) )
+            {
+                using (StreamWriter sw = new StreamWriter(file))
+                {
+                    foreach (string item in queue)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
